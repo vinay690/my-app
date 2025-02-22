0e9d6c37ffb707605c9947e6b0413ae9025ea263
vk <vk.com>
2025-02-22 12:33:27 +0530

    created basic node index and test file

---
+++ tests/server.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /', () => {
+    it('should return Hello, GitHub Actions!', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toEqual('Hello, GitHub Actions!');
+    });
+});
